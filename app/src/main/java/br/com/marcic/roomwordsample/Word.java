9a102f779d1f055562fc4ec7369761064df0e46b
Carlos Andrade <carlosandradedev@gmail.com>
2019-12-28 23:43:11 -0300

    creating the entity

---
+++ app/src/main/java/br/com/marcic/roomwordsample/Word.java
@@ -0,0 +1,23 @@
+package br.com.marcic.roomwordsample;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public Word(String mWord) {
+        this.mWord = mWord;
+    }
+
+    public String getmWord() {
+        return this.mWord;
+    }
+}
